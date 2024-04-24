9196c836d0257b889747144ec2a895e5141b2160
Vitaliy Murzin <murzinvit@yandex.ru>
2024-04-24 15:28:02 +0500

    init commit 1

--- src/Accounts/SimpleAccount.java
+++ src/Accounts/SimpleAccount.java
@@ -1,4 +1,34 @@
 package Accounts;
 
 public class SimpleAccount extends Account {
+    private int amount;
+
+    @Override
+    boolean add(long amount) {
+        if (amount > 0) {
+            this.amount += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean pay(long amount) {
+        if (amount < 0 || this.amount >= amount) {
+            this.amount -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean transfer(Account account, long amount) {
+        if(pay(amount)) {
+            account.add(amount);
+        }
+    }
+
+    long getBalance() {
+        return amount;
+    }
 }
